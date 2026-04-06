1416da13e518df6f2f2a0db499e4102503160d1a
molha509 <molha509@gmail.com>
2026-04-06 05:31:47 -0400

    Create server.js

---
+++ server.js
@@ -0,0 +1,16 @@
+// server.js
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// GitHub webhook route
+app.post("/github-webhook", (req, res) => {
+    console.log("📦 GitHub webhook triggered:", req.body);
+    res.status(200).send("OK");
+});
+
+const PORT = 3000;
+app.listen(PORT, () => console.log(`Node.js API running on port ${PORT}`));
