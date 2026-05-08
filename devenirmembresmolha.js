e5022920e735545fe7d57abeec847cd1ac51c978
molha509 <molha509@gmail.com>
2026-05-08 17:27:55 -0400

    Update devenirmembresmolha.js

--- devenirmembresmolha.js
+++ devenirmembresmolha.js
@@ -68,3 +68,21 @@ const form = event.target; // ✅ SA OU TE BLIYE A
         alert("Impossible de contacter le serveur.");
     }
 }
+
+
+const dateInput = document.getElementById("datenaissance");
+
+dateInput.addEventListener("input", function (e) {
+
+    let value = e.target.value.replace(/\D/g, "");
+
+    if (value.length > 2) {
+        value = value.slice(0,2) + "/" + value.slice(2);
+    }
+
+    if (value.length > 5) {
+        value = value.slice(0,5) + "/" + value.slice(5,9);
+    }
+
+    e.target.value = value;
+});
